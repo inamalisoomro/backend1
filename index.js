2face355132061ba0ff689eb801c8e6311db7819
INAMALiSOOMRO <inamalisoomro90@gmail.com>
2026-07-12 04:46:10 +0500

    backend cors

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'authorization', 'Origin', 'Accept']
+}));
 app.use(express.json())
 app.use(cookieParser());
 
